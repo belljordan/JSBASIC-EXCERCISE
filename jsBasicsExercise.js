21bea5d299c8bb7a0cc5bdb8fb9e19a278fd25f1
Jordan Bell <jordanbell@MacBook-Pro-100.local>
2020-10-12 13:43:19 -0500

    Created jsBasics Excercise.js

---
+++ jsBasicsExercise.js
@@ -0,0 +1,26 @@
+console.log(17);
+/*
+1.Numbers
+2.Strings
+3.Booleans
+4.Null
+5.Undefined
+*/
+/*
+1.Addition (+)
+2. Subtraction (-)
+3. Multiplication (*)
+4. Division(/)
+5.Modulo (%)
+*/
+console.log(7%4);
+console.log(7% -4);
+console.log('I\'M MARY POPPINS Y\'ALL');
+console.log("SPIDER" + "-MAN");
+console.log("Jordan".length + "Bell".length) 
+console.log("Avengers: Endgame"[10]);
+console.log("\\");
+console.log(Math.pow(5,4)-9);
+console.log("I".length - "Am".length + "Iron".length - "Man".length);
+console.log("CAP"[1] +"VISION"[0] +"HAWKEYE"[4] +"NICK FURY"[0] +"GROOT"[0] +"DOCTOR STRANGE"[13] +"THOR"[3] +"STAR-LORD"[0]);
+
